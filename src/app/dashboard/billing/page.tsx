6a328badc79baf87ed06f6f7d8707217199fb135
Nithul k <nithulk2003@gmail.com>
2023-10-01 11:54:53 +0530

    testing commit

--- src/app/dashboard/billing/page.tsx
+++ src/app/dashboard/billing/page.tsx
@@ -1,6 +1,6 @@
 import BillingForm from "@/components/BillingForm";
 import { getUserSubscriptionPlan } from "@/lib/stripe";
-
+//test
 const Page = async () => {
   const subscriptionPlan = await getUserSubscriptionPlan();
 
